ca9000b8bae2afd426a2ec3e474410131217f035
ninjabsbx <ninjaabsbx@gmail.com>
2023-06-23 18:03:23 -0300

    .

---
+++ LogicadePromacao2/ConsoleApp1/Exercicio2.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite valores para somar a media aritmetica");
+double[] A = new double[N];
+double media = 0;
+for (int i = 0; i < N; i++)
+{
+    A[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    media += A[i];
+}
+media /= N;
+Console.WriteLine("Media:");
+Console.WriteLine(media);
+for (int i = 0; i < N; i++)
+{
+    if (A[i] < media)
+        Console.WriteLine(A[i]);
+}
+
+
+
+
---
+++ LogicadePromacao2/LogicadePromacao2/Exercicio1.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+Console.WriteLine("Digite quantos valores para colocar");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite valores para o Vetor A");
+double[] A = new double[N];
+double[] B = new double[N];
+double[] C = new double[N];
+for (int i = 0; i < N; i++) 
+{
+    A[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+Console.WriteLine("Digite valores para o Vetor B");
+for (int i = 0; i < N; i++)
+{
+    B[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    C[i] = A[i] + B[i];
+}
+String C_str = "C: ";
+for (int i = 0; i < N; i++)
+{
+    C_str += C[i] + " ";
+}
+Console.WriteLine(C_str);
